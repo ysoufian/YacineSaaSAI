99612a13c424fe64bacc4d746bcdcfce1d4bd364
ysoufian <ysoufian@vt.edu>
2023-12-31 21:50:24 -0500

    added app insight key

--- client/src/AppInsights.js
+++ client/src/AppInsights.js
@@ -1,10 +1,13 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
 
+
+
 var reactPlugin = new ReactPlugin();
 var appInsights = new ApplicationInsights({
     config: {
-        connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
+        connectionString: "InstrumentationKey=fb63e2e5-a5d1-444a-89bc-c8e116ea3695;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"
+        ,
         enableAutoRouteTracking: true,
         extensions: [reactPlugin]
     }
